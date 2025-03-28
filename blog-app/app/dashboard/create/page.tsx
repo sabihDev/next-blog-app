2855cfca2842a4ecc3784016686c63453ec790c1
sabihDev <8203sabi.rahman@gmail.com>
2025-03-28 10:36:12 +0500

    Creating blog api created

--- blog-app/app/dashboard/create/page.tsx
+++ blog-app/app/dashboard/create/page.tsx
@@ -1,3 +1,4 @@
+import { createPost } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -14,48 +15,53 @@ export default function CreatePosts() {
   return (
     <div className="container mx-auto py-10">
       <Card className="max-w-2xl mx-auto shadow-lg">
-      <CardHeader className="space-y-2">
-        <CardTitle className="text-3xl font-bold text-center">Create Post</CardTitle>
-        <CardDescription className="text-center text-gray-500">
-        Share your thoughts and ideas with the world
-        </CardDescription>
-      </CardHeader>
+        <CardHeader className="space-y-2">
+          <CardTitle className="text-3xl font-bold text-center">
+            Create Post
+          </CardTitle>
+          <CardDescription className="text-center text-gray-500">
+            Share your thoughts and ideas with the world
+          </CardDescription>
+        </CardHeader>
 
-      <CardContent>
-        <form className="space-y-6">
-        <div className="space-y-2">
-          <Label className="text-sm font-medium">Post Title</Label>
-          <Input 
-          placeholder="Enter your post title..."
-          className="w-full p-3"
-          type="text"
-          required
-          />
-        </div>
+        <CardContent>
+          <form className="space-y-6" action={createPost}>
+            <div className="space-y-2">
+              <Label className="text-sm font-medium">Post Title</Label>
+              <Input
+                placeholder="Enter your post title..."
+                className="w-full p-3"
+                name="title"
+                type="text"
+                required
+              />
+            </div>
 
-        <div className="space-y-2">
-          <Label className="text-sm font-medium">Content</Label>
-          <Textarea 
-          placeholder="Write your post content here..."
-          className="min-h-[150px] w-full p-3"
-          required
-          />
-        </div>
+            <div className="space-y-2">
+              <Label className="text-sm font-medium">Content</Label>
+              <Textarea
+                placeholder="Write your post content here..."
+                className="min-h-[150px] w-full p-3"
+                name="content"
+                required
+              />
+            </div>
 
-        <div className="space-y-2">
-          <Label className="text-sm font-medium">Cover Image</Label>
-          <Input
-          type="url"
-          className="w-full"
-          placeholder="Image url"
-          />
-        </div>
+            <div className="space-y-2">
+              <Label className="text-sm font-medium">Cover Image</Label>
+              <Input
+                type="url"
+                className="w-full"
+                name="url"
+                placeholder="Image url"
+              />
+            </div>
 
-        <Button className="w-full py-5 text-lg font-semibold">
-          Publish Post
-        </Button>
-        </form>
-      </CardContent>
+            <Button className="w-full py-5 text-lg font-semibold">
+              Publish Post
+            </Button>
+          </form>
+        </CardContent>
       </Card>
     </div>
   );
